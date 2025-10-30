15439dbf4c5510e5061e86ccc55aa5ebe41c45da
Zafarali Tolibov <zafaralit67@gmail.com>
2025-10-30 18:58:47 -0500

    Implemented relationships between entities User, Tag, and Address

--- src/main/java/com/crxs/demo_store/entities/Tag.java
+++ src/main/java/com/crxs/demo_store/entities/Tag.java
@@ -3,11 +3,13 @@ package com.crxs.demo_store.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
 @Setter
-@AllArgsConstructor
+@ToString
 @NoArgsConstructor
-@Builder
 @Entity
 @Table(name = "tags")
 public class Tag {
@@ -18,4 +20,12 @@ public class Tag {
 
     @Column(nullable = false, name = "name")
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    @ToString.Exclude
+    private Set<User> users = new HashSet<>();
+
+    public Tag(String name) {
+        this.name = name;
+    }
 }
