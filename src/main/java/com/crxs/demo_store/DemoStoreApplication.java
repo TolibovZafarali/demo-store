85dd92bb8f0557322f81bc666b636a8b5bb04c3e
Zafarali Tolibov <zafaralit67@gmail.com>
2025-10-30 14:39:43 -0500

    Initial commit

---
+++ src/main/java/com/crxs/demo_store/DemoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.crxs.demo_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crxs/demo_store/DemoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crxs.demo_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
