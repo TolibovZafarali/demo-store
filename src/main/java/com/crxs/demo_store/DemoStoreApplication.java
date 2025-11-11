35ebc0d0336cd7d3c734f8fea76ceb3782dc1e00
Zafarali Tolibov <zafaralit67@gmail.com>
2025-11-11 11:33:08 -0600

    Learning Lazy and Eager initialization

--- src/main/java/com/crxs/demo_store/DemoStoreApplication.java
+++ src/main/java/com/crxs/demo_store/DemoStoreApplication.java
@@ -1,13 +1,20 @@
 package com.crxs.demo_store;
 
+import com.crxs.demo_store.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoStoreApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DemoStoreApplication.class, args);
+		var service = context.getBean(UserService.class);
+
+//		service.showEntityStates();
+//		service.showRelatedEntities();
+		service.fetchAddress();
 	}
 
 }
